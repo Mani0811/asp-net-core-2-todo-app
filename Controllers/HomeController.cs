bbca1a57306b1197d14e01a2e58333e8740b34ba
Mani Shankar <mani.shankar@MediaKind.Com>
2020-04-03 17:12:07 +0530

    intial draft added

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -5,14 +5,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using AspNetCoreTodo.Models;
+using RestSharp;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace AspNetCoreTodo.Controllers
 {
     public class HomeController : Controller
     {
+        private RestClient client = new RestClient("https://api.covid19india.org/");
+
         public IActionResult Index()
         {
-            return View();
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create("https://api.covid19india.org/data.json");
+            request.Method = "GET";
+            String test = String.Empty;
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                Stream dataStream = response.GetResponseStream();
+                StreamReader reader = new StreamReader(dataStream);
+                test = reader.ReadToEnd();
+                reader.Close();
+                dataStream.Close();
+            }
+
+            NationDataModel nationDataModel = JsonConvert.DeserializeObject<NationDataModel>(test);
+
+            return View(nationDataModel);
         }
 
         public IActionResult About()
